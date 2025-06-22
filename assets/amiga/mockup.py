6bd1f56dc633581a7c0b55fb8ce85df10e86ed7e
Jean-François Fabre <jotd666@gmail.com>
2025-06-22 15:47:43 +0200

    ready to log sprites

--- assets/amiga/mockup.py
+++ assets/amiga/mockup.py
@@ -19,8 +19,8 @@ def raw_to_code(raw):
     return base
 
 
-def load_tileset(image_name,width,height,set_subdir,dump_prefix=""):
-    full_image_path = sheets_path / "sprites" / set_subdir / image_name
+def load_tileset(image_name,width,height,dump_prefix=""):
+    full_image_path = sheets_path / "sprites" / image_name
     tiles_1 = Image.open(full_image_path)
     nb_rows = tiles_1.size[1] // height
     nb_cols = tiles_1.size[0] // width
@@ -43,85 +43,52 @@ def load_tileset(image_name,width,height,set_subdir,dump_prefix=""):
 
     return tileset_1
 
-def loadtiles(i,set_name):
-    return load_tileset(f"pal_{i:02x}.png",16,8,set_name)
+def loadtiles(i):
+    return load_tileset(f"pal_{i:02x}.png",16,16)
 
-tile_sets_0 = [loadtiles(i,"set_0") for i in range(16)]
-tile_sets_1 = [loadtiles(i,"set_1") for i in range(16)]
-tile_set = [tile_sets_0,tile_sets_1]
+tile_set = [loadtiles(i) for i in range(16)]
 
-def process(the_dump,offset=0,base_address=0,name_filter=None,hide_named_sprite=None):
+
+def process(the_dump,name_filter=None,hide_named_sprite=None):
     the_dump = pathlib.Path(the_dump)
     # in input, we use a MAME memory dump: save sprites,$A000,$400
     # (0x200 are read, but there's a kind of double buffering
     with open(the_dump,"rb") as f:
-        f.read(offset)
-        m_spriteram = bytearray(f.read(0x200))
+        mem_1800 = bytearray(f.read())
 
+    spriteram = mem_1800[0x400:0x440]
+    spriteram_2 = mem_1800[:0x40]
 
     result = Image.new("RGB",(256,256))
 
     print("*"*50)
     nb_active = 0
-    nb_stars = 0
-
-
-    for offs in range(len(m_spriteram)-4,-4,-4):
-        raw_code_and_clut = (m_spriteram[offs+1]<<8)+m_spriteram[offs+2]
-        x = m_spriteram[offs]
-        y = 241 - m_spriteram[offs + 3]
-        y = 256 -y # adding this to MAME formula else pic is not correct
-
-        if x:
-            gfx_bank = m_spriteram[offs + 1] & 0x01
-            code = ((m_spriteram[offs + 2] & 0x20) << 2) | (m_spriteram[offs + 1] >> 1)
-            if code:
-                if code == 0x13:
-                    nb_stars += 1
-                color = m_spriteram[offs + 2] & 0x0f
-                flip_x = bool(~m_spriteram[offs + 2] & 0x40)
-                flip_y = bool(m_spriteram[offs + 2] & 0x80)
-                nb_active += 1
-
-
-                address = base_address+offs
-                fcode = gfx_bank*0x100+code
-                name = sprite_names.get(fcode,"unknown")
-                if not name_filter or name_filter in name:
-                    print(f"addr=${address:04x}, offset=${offs:04x}, raw:=${raw_code_and_clut:04X} X={y}, Y={x}, flipx={flip_x}, flip_y={flip_y}, code={fcode:04x}, color={color:02x}, name={name}")
-
-                bank = tile_set[gfx_bank]
-                im = bank[color][code]
-
-                if flip_x:
-                    im = ImageOps.flip(im)
-                if flip_y:
-                    im = ImageOps.mirror(im)
-
-    ##
-    ##            for x in range(img.size[0]):
-    ##                for y in range(img.size[1]):
-    ##                    if img.getpixel((x,y)) == (0,0,0,255):
-    ##                        img.putpixel((x,y),(255,0,0,0))
-                # paste would require masking, well, never mind
-                if hide_named_sprite and hide_named_sprite in name:
-                    pass
-                else:
-                    result.paste(im,(y,x))
-
-    print(f"NB ACTIVE: {nb_active}, NB_STARS: {nb_stars}")
-    result.save(f"{the_dump.stem}_{offset:04x}.png")
 
 
 
-process(r"sprite_ram_4040",offset=0,base_address=0x4040,hide_named_sprite="star")
-#process(r"sprites_4040",offset=0,base_address=0xA000,name_filter="swarm")
-#process(r"sprites_A200",offset=0,base_address=0xA000)
-#process(r"sattelites_A000",offset=0,base_address=0xA000)
-#process(r"bug_4040",offset=0,base_address=0x4040)
-#process(r"../../sprite_ram_4040",offset=0,base_address=0x4040)
-#process(r"../../sprite_ram_A000",offset=0,base_address=0xA000)
-#process(r"../../sprite_ram_A200",offset=0,base_address=0xA200)
-#process(r"gysub_6000",offset=0x0)
-#process(r"gysub_after_6000_amiga",offset=0)
-#process(r"gysub_after_6000",offset=0x200)
+    for offs in range(0X3E,-2,-2):
+        if spriteram[offs] and spriteram_2[offs + 1]:
+            attr = spriteram_2[offs]
+            color = attr & 0x0f
+            code = spriteram[offs + 1]
+            flipx = ~attr & 0x40
+            flipy = attr & 0x80
+            sx = spriteram[offs] - 1
+            sy = 240-spriteram_2[offs + 1] + 1
+
+            im = tile_set[color][code]
+
+            if flipx:
+                im = ImageOps.flip(im)
+            if flipy:
+                im = ImageOps.mirror(im)
+
+            print(f"code:{code:02x}, color:{color:02x}, X:{sx}, Y:{sy}")
+            result.paste(im,(sx,sy))
+            nb_active += 1
+
+    result.save(f"{the_dump.stem}.png")
+    print(f"nb active: {nb_active}")
+
+
+process(r"mem_1800")
